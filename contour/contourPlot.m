f87435d6d36057613ebf0f0bc36ef4ad27954dc9
Linxson <964941936@qq.com>
2022-05-27 16:56:56 +0800

    add level

--- contour/contourPlot.m
+++ contour/contourPlot.m
@@ -1,13 +1,14 @@
 clc;clear;close all;
 % 数据
+level=[15,5,60];
 box=[20000,3000,35000;6000,3000,14000];
 % 高程
 contourMap('../data/data_max.xlsx',...
     '../data/contourM.xlsx',[-5,5,50],box,1,1);
 % 等水压线
 contourMap('../data/data_max.xlsx',...
-    '../data/contourM.xlsx',[15,5,60],box,2,0);
+    '../data/contourM.xlsx',level,box,2,0);
 contourMap('../data/data_fire.xlsx',...
-    '../data/contourM.xlsx',[15,5,60],box,3,0);
+    '../data/contourM.xlsx',level,box,3,0);
 contourMap('../data/data_acc.xlsx',...
-    '../data/contourM.xlsx',[15,5,60],box,4,0);
+    '../data/contourM.xlsx',level,box,4,0);
